34643e4bea79dce20e19f1a68e8678cfc2eb0a9e
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-08-16 15:38:00 -0300

    initial commit

---
+++ src/database/config.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2/promise";
+
+const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "db_library"
+});
