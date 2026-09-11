ef853138aa4d683f5b4a5d28b9f3675f922f1342
Victor-Policarpo <victorpolicarposilva@gmail.com>
2026-09-11 15:00:37 -0300

    feat: enable cors for frontend integration

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import libraryRoutes from "./routes/libraryRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -7,6 +8,18 @@ import loanRoutes from "./routes/loanRoutes.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
 
 const app = express();
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const allowedOrigins = ["http://localhost:5173"];
+        if (!origin || allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/auth", authRoutes);
